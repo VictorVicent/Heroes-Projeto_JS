1665993424b6e05b8d144d403242ed9ca1ef8196
PedroEBC <116644843+PedroEBC@users.noreply.github.com>
2022-11-26 15:37:22 -0300

    Adicionando Dom
    Dom dos filmes Thor: Ragnarok e Guardiões da Galáxia.

--- main.js
+++ main.js
@@ -46,3 +46,25 @@ function alterarConteudo7() {
 function alterarConteudo8() {
   document.getElementsByClassName("words")[3].innerHTML = "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade."
 }
+
+//Thor: Ragnarok//
+function alterarConteudo9() {
+  document.getElementsByClassName("words")[4].innerHTML = "<img src='avimg/6.png' height='130px'>"
+}
+
+
+  
+function alterarConteudo10() {
+  document.getElementsByClassName("words")[4].innerHTML = "Após anos afastado, Thor retorna para casa e descobre que seu pai, Odin, rei de Asgard, está desaparecido. Após encontrá-lo, ele toma conhecimento de sua irmã mais velha, Hela, a poderosa e implacável deusa da morte. Com o auxílio de Loki, ele enfrenta Hela, mas durante a batalha, Thor acaba preso em Sakaar, um planeta do outro lado do universo.Agora, ele precisa correr contra o tempo para voltar a Asgard e impedir o Ragnarok, a destruição de seu mundo."
+}
+
+//Guardiões da Galaxia//
+function alterarConteudo11() {
+  document.getElementsByClassName("words")[5].innerHTML = "<img src='avimg/5.png' height='130px'>"
+}
+
+
+  
+function alterarConteudo12() {
+  document.getElementsByClassName("words")[5].innerHTML = "O aventureiro do espaço Peter Quill torna-se presa de caçadores de recompensas depois que rouba a esfera de um vilão traiçoeiro, Ronan. Para escapar do perigo, ele faz uma aliança com um grupo de quatro extraterrestres.Quando Quill descobre que a esfera roubada possui um poder capaz de mudar os rumos do universo, ele e seu grupo deverão proteger o objeto para salvar o futuro da galáxia."
+}
