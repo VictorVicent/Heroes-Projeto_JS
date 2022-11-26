157e6cbb26923283cc2629b4f02428bd64b4e59b
PedroEBC <116644843+PedroEBC@users.noreply.github.com>
2022-11-26 15:34:58 -0300

    Adicionando dom
    Dom dos filmes Pantera Negra e Vingadores.

--- main.js
+++ main.js
@@ -27,3 +27,22 @@ function alterarConteudo3() {
 function alterarConteudo4() {
   document.getElementsByClassName("words")[1].innerHTML = "Depois do ataque de Ultron, os políticos decidem controlar os Vingadores, já que suas ações afetam toda a humanidade.A decisão coloca o Capitão América em rota de colisão com o Homem de Ferro."
 }
+
+//Pantera Negra//
+function alterarConteudo5() {
+  document.getElementsByClassName("words")[2].innerHTML = "<img src='avimg/8.png' height='130px'>"
+}
+
+
+function alterarConteudo6() {
+  document.getElementsByClassName("words")[2].innerHTML = "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores.Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados."
+}
+
+//Vingadores//
+function alterarConteudo7() {
+  document.getElementsByClassName("words")[3].innerHTML = "<img src='avimg/7.png' height='130px'>"
+}
+
+function alterarConteudo8() {
+  document.getElementsByClassName("words")[3].innerHTML = "Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.Homem de Ferro, Capitão América, Hulk, Thor, Viúva Negra e Gavião Arqueiro formam o time dos sonhos de Fury, mas eles precisam aprender a colocar os egos de lado e agir como um grupo em prol da humanidade."
+}
